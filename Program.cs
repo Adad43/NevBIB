8776b7e0f373df5e1b1fe42e2917a3531c831de7
Adad43 <118421216+Adad43@users.noreply.github.com>
2022-11-21 15:31:15 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BIB1;
+
+namespace Создание_билиотеки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person tom = new("");
+            tom.Print();
+        }
+    }
+}
